fd62b0247420f94f292a6b63eabd20a5ff5e576c
geb <_>
2018-09-03 23:41:39 +0100

    update service worker

--- sw.js
+++ sw.js
@@ -1,11 +1,18 @@
 const CACHE_NAME = "CUCC_CACHE";
 
+console.log = function(...items) {
+    items.forEach((item, i) => {
+        items[i] = (typeof item === 'object' ? JSON.stringify(item,null,4) : item);
+    });
+    output.innerHTML += items.join(' ') + '<br />';
+};
+
 self.addEventListener("install", function(event) {
     console.log("installing");
     let offline_request = new Request("/ccuc/offline.html");
     event.waitUntil(fetch(offline_request).then(function(response) {
         return caches.open(CACHE_NAME).then(function(cache) {
-            console.log("cachine offline page: ", response);
+            console.log("cachine offline page");
             return cache.put(offline_request, response);
         });
     }));
