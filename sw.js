caf9c6a8943bf616c37d73fe49d7cda222d04b9a
geb <_>
2018-09-03 23:47:51 +0100

    update service worker

--- sw.js
+++ sw.js
@@ -1,29 +1,22 @@
 const CACHE_NAME = "CUCC_CACHE";
 
-clog = function(...items) {
-    items.forEach((item, i) => {
-        items[i] = (typeof item === 'object' ? JSON.stringify(item,null,4) : item);
-    });
-    document.body.innerHTML += items.join(' ') + '<br />';
-};
-
 self.addEventListener("install", function(event) {
     console.log("installing");
     let offline_request = new Request("/ccuc/offline.html");
     event.waitUntil(fetch(offline_request).then(function(response) {
         return caches.open(CACHE_NAME).then(function(cache) {
-            clog("cachine offline page");
+            console.log("cachine offline page");
             return cache.put(offline_request, response);
         });
     }));
 });
 
 self.addEventListener("fetch", function(event) {
-    clog("fetching");
+    console.log("fetching");
     let request = event.request;
     if (request.method === "GET") {
         event.respondWith(fetch(request).catch(function(error) {
-            clog("offline. serving offline page");
+            console.log("offline. serving offline page");
             return caches.open(CACHE_NAME).then(function(cache) {
                 return cache.match("offline.html");
             });
@@ -32,7 +25,7 @@ self.addEventListener("fetch", function(event) {
 });
 
 self.addEventListener("activate", function(event) {
-    clog("activating");
+    console.log("activating");
     event.waitUntil(caches.keys().then(function(cache_names) {
         return Promise.all(cache_names.map(function(cache_name) {
             caches.delete(cache_name);
