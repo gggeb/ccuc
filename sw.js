442778795f26aa9982c6970af7608975a0cf4e6a
geb <_>
2018-09-03 22:00:00 +0100

    update service worker

--- sw.js
+++ sw.js
@@ -7,57 +7,49 @@ let urls = [
 ];
 
 self.addEventListener("install", function(event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function(cache) {
-                console.log("cache opened");
-                return cache.addAll(urls);
-            }
-        )
-    );
+    event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
+        console.log("cache opened");
+        return cache.addAll(urls);
+    }));
 });
 
 self.addEventListener("fetch", function(event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                console.log("fetching: ", event.request.clone());
-                if (response) {
+    console.log("yo");
+    event.respondWith(caches.match(event.request).then(function(response) {
+        console.log("fetching: ", event.request.clone());
+        if (response) {
+            return response;
+        } else {
+            let fetch_request = event.request.clone();
+
+            return fetch(fetch_request).then(function(response) {
+                if (!reponse || response.status !== 200 ||
+                    response.type !== "basic") {
                     return response;
-                } else {
-                    let fetch_request = event.request.clone();
-
-                    return fetch(fetch_request).then(function(response) {
-                        if (!reponse || response.status !== 200 ||
-                            response.type !== "basic") {
-                            return response;
-                        }
-
-                        var response_to_cache = response.clone();
-
-                        caches.open(CACHE_NAME).then(function(cache) {
-                            cache.put(event.request, response_to_cache);
-                        });
-
-                        return response;
-                    });
                 }
-            })
-    );
+
+                var response_to_cache = response.clone();
+
+                caches.open(CACHE_NAME).then(function(cache) {
+                    cache.put(event.request, response_to_cache);
+                });
+
+                return response;
+            });
+        }
+    }));
 });
 
 self.addEventListener("activate", function(event) {
     var whitelist = [];
     console.log("activating");
-    event.waitUntil(
-        caches.keys().then(function(cache_names) {
-            return Promise.all(
-                cache_names.map(function(cache_name) {
-                    if (whitelist.indexOf(cache_name)) {
-                        return caches.delete(cache_name);
-                    }
-                })
-            );
-        })
-    );
+    event.waitUntil(caches.keys().then(function(cache_names) {
+        return Promise.all(
+            cache_names.map(function(cache_name) {
+                if (whitelist.indexOf(cache_name)) {
+                    return caches.delete(cache_name);
+                }
+            })
+        );
+    }));
 });
